d3ce55323df46debda77323b369e936829f25ef6
Tiago França <tiagofranca.rita@gmail.com>
2023-07-18 21:32:34 -0300

    validacao do formulario de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from 'src/app/models/credeciais';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email)
+  senha = new FormControl(null, Validators.minLength(3))
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  validarCampos(): boolean {
+    if(this.email.valid && this.senha.valid){
+      return true;
+    } else{
+      return false;
+    }
+  }
 }
